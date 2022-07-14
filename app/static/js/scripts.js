e9fb910598df02eeab5d2ea792f9527d6790e5b3
Siqi-Fang <sfang@hamilton.edu>
2022-07-14 17:28:35 +0000

    init sync

---
+++ app/static/js/scripts.js
@@ -0,0 +1,45 @@
+/*!
+* Start Bootstrap - One Page Wonder v6.0.5 (https://startbootstrap.com/theme/one-page-wonder)
+* Copyright 2013-2022 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-one-page-wonder/blob/master/LICENSE)
+*/
+// This file is intentionally blank
+// Use this file to add JavaScript to your project
+const context = new window.AudioContext();
+
+function play_audio(){
+  console.log("BUTTON PRESSED")
+//     const successNoise = context.createOscillator();
+//     successNoise.frequency = "600";
+//     successNoise.type = "sine";
+//     successNoise.frequency.exponentialRampToValueAtTime(
+//         800,
+//         context.currentTime + 0.05
+//     );
+//     successNoise.frequency.exponentialRampToValueAtTime(
+//         1000,
+//         context.currentTime + 0.15
+//     );
+
+//     successGain = context.createGain();
+//     successGain.gain.exponentialRampToValueAtTime(
+//         0.01,
+//         context.currentTime + 0.3
+//     );
+
+//     successFilter = context.createBiquadFilter("bandpass");
+//     successFilter.Q = 0.01;
+
+//     successNoise
+//         .connect(successFilter)
+//         .connect(successGain)
+//         .connect(context.destination);
+//     successNoise.start();
+//     successNoise.stop(context.currentTime + 0.2);
+}
+
+
+let submitButton = document.querySelector("#scroll");
+submitButton.addEventListener("click",function(){
+  play_audio()
+})
